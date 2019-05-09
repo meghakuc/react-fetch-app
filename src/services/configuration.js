bfd16a94288553b87b9ea1e09c0383fb94992220
meghakuc <meghakuchhal@gmail.com>
2019-05-09 13:43:56 +0530

    Add files via upload

---
+++ src/services/configuration.js
@@ -0,0 +1,9 @@
+class Configuration {
+
+	ALL_PRODUCTS_URL = 'https://app.dataweave.com/v6/app/retailer/bundles/?&base_view=&start=&limit=&sort_on=&sort_by=&filters={"search":""}&api_key=38430b87ac715c5858b7de91fb90b3f7';
+	MATCHED_ALL_PRODUCTS_URL = 'https://app.dataweave.com/v6/app/retailer/bundles/?&base_view=all_products&api_key=38430b87ac715c5858b7de91fb90b3f7';
+	MARGIN_GAIN_URL = 'https://app.dataweave.com/v6/app/retailer/bundles/?&base_view=increase_opportunity&api_key=38430b87ac715c5858b7de91fb90b3f7';
+	ITEM_DETAIL_URL = 'https://app.dataweave.com/v6/app/retailer/bundle_overview/?&api_key=38430b87ac715c5858b7de91fb90b3f7&base_view=all_products&bundle_id=';
+}
+
+export default Configuration;
